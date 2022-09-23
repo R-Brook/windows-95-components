1b2f69933e290d196ce12a934a88666db533ed05
R Brook <rlbrook@gmail.com>
2022-09-23 20:23:38 +0100

    [Feature] Finish building basic primary button and button story.

--- components/buttons/index.tsx
+++ components/buttons/index.tsx
@@ -1,13 +1,20 @@
 import React, { FC } from "react"
 
 export interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {
-  primary: boolean
+  width: "wide" | "tight"
 }
 
-export const Button: FC<ButtonProps> = (
-  {
-    //
-  }
-) => {
-  return <button className="">Button</button>
+export const Button: FC<ButtonProps> = ({ width, children }) => {
+  return (
+    <button
+      className={
+        "bg-gray border-4 border-t-gray-light border-l-gray-light border-r-black border-b-black" +
+        " " +
+        (width === "wide" ? " px-6 " : "px-2")
+      }
+    >
+      {children}
+    </button>
+  )
 }
+// px-10 py-1
