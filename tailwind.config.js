cdc8d4e88126384ae31f43597ebc130bb0c70c02
R Brook <rlbrook@gmail.com>
2022-09-23 19:39:28 +0100

    [init] Tailwind colour setup, add font and Windows start logo.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      blue: "#1fb6ff",
+      black: "#000000",
+      orange: "#ff7849",
+      green: "#13ce66",
+      "green-blue": "#008080",
+      "gray-dark": "#273444",
+      gray: "#C0C0C0",
+      "gray-light": "#E5EEED",
+    },
   },
   plugins: [],
 }
