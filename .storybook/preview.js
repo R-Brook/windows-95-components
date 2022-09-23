961b379f1211eb59ec7c8a2afbe3965466a67c78
R Brook <rlbrook@gmail.com>
2022-09-23 19:35:51 +0100

    [init] Finish Storybook setup.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,25 @@
 import "../css/globals.css"
 
+import * as NextImage from "next/image"
+
+const OriginalNextImage = NextImage.default
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+})
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  backgrounds: {
+    default: "desktop",
+    values: [
+      {
+        name: "desktop",
+        value: "#008080",
+      },
+    ],
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
