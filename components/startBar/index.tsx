1abcf99d6357f562d0865bb8f55245e00684b4a4
R Brook <rlbrook@gmail.com>
2023-01-29 19:18:40 +0000

    [chore] Reorder Tailwind startbar classes.

--- components/startBar/index.tsx
+++ components/startBar/index.tsx
@@ -9,7 +9,7 @@ export interface StartBarProps {
 
 export const StartBar: FC<StartBarProps> = ({}) => {
   return (
-    <div className="bg-gray border-2 border-t-gray-light border-gray inset-0 flex max-w-screen-lg m-auto">
+    <div className="bg-gray border-2 border-t-grey-green border-gray inset-0 flex max-w-screen-lg m-auto">
       <Button width="tight" className="border-0 px-2 mt-0.5 flex items-center">
         <span className="relative block h-6 w-8 my-1 mr-1 mx-0.5">
           <Image
@@ -43,7 +43,7 @@ export const StartBar: FC<StartBarProps> = ({}) => {
         programmes
       </div>
       <VerticalDivider />
-      <div className="block bg-gray border-2 border-b-gray-light border-r-gray-light border-l-gray-dark border-t-gray-dark m-0 my-0.5 px-3 py-0.5 text-xl flex-initial">
+      <div className="block bg-gray border-2 border-b-grey-green border-r-grey-green border-l-grey-dark border-t-grey-dark m-0 my-0.5 px-3 py-0.5 text-xl flex-initial">
         8:06 PM
       </div>
     </div>
